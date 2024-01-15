802ba3159f70f9c895e2f36f6925557dfbaa2dc9
Yongveloper <aaa9015@naver.com>
2024-01-16 00:24:14 +0900

    feat: 인터셉팅 라우트

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -8,7 +8,6 @@ interface ILayoutProps {
 export default function Layout({ children, modal }: ILayoutProps) {
   return (
     <div className={styles.container}>
-      비포 로그인 레이아웃
       {children}
       {modal}
     </div>
